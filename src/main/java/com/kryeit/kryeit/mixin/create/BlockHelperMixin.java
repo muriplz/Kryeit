a4fb903e87d7c0ff94f84538537a2aa21813292e
muriplz <46825658+muriplz@users.noreply.github.com>
2024-07-30 16:27:37 +0200

    temp fix for schematicannon crash

---
+++ src/main/java/com/kryeit/kryeit/mixin/create/BlockHelperMixin.java
@@ -0,0 +1,35 @@
+package com.kryeit.kryeit.mixin.create;
+
+import com.simibubi.create.foundation.utility.BlockHelper;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockHelper.class)
+public class BlockHelperMixin {
+
+	@Inject(
+			method = "placeSchematicBlock",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/block/entity/BlockEntity;createFromNbt(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/block/entity/BlockEntity;",
+					shift = At.Shift.AFTER
+			),
+			cancellable = true
+	)
+	private static void onPlaceSchematicBlock(World world, BlockState state, BlockPos target, ItemStack stack, NbtCompound data, CallbackInfo ci) {
+		BlockEntity loaded = BlockEntity.createFromNbt(target, state, data);
+		if (loaded != null) {
+			ci.cancel();
+		}
+	}
+}
